5d3f8896047baa1959742e9db9cc90d800af770e
Ayush Raj <ayush04060@gmail.com>
2026-09-02 21:40:24 +0530

    The Show composes ShowSeat objects – they are created from the Screen’s seats and exist only for this show. findShowSeat returns the ShowSeat for a given seat number (or null if invalid). displayLayout prints a visual map – we’ll use this in the menu.

--- ShowtimeBooking/src/Show.java
+++ ShowtimeBooking/src/Show.java
@@ -14,6 +14,12 @@ public class Show {
         this.screen = screen;
         this.startTime = startTime;
         this.showSeats = new ArrayList<>();
+        //create a ShowSeat for every physical seat in the screen
+        for(Seat seat : screen.getSeats())
+        {
+            ShowSeat showSeat = new ShowSeat(seat);
+            showSeats.add(showSeat);
+        }
     }
 
     public Movie getMovie()
@@ -32,6 +38,30 @@ public class Show {
         return showSeats;
     }
     public ShowSeat findShowSeat(String seatNumber){
+        for(ShowSeat ss : showSeats)
+        {
+            if (ss.getSeat().getSeatNumber().equals(seatNumber))
+            {
+                return ss;
+            }
+        }
         return null;
+    }
+
+    public void  displayLayout()
+    {
+        System.out.println("Seat layout (" + screen.getScreenNumber() + "):");
+        for (ShowSeat ss :showSeats)
+        {
+            String marker = ss.isAvailable() ? "[ ]" : "[X]";
+            System.out.println(ss.getSeat().getSeatNumber() + marker + " " );
+            //Line break after every 5 seats to make it look better
+            if (ss.getSeat().getSeatNumber().length() == 2 &&
+            ss.getSeat().getSeatNumber().charAt(1) == '5') {
+                System.out.println();
+            }
+        }
+        System.out.println();
+
     }
 }
