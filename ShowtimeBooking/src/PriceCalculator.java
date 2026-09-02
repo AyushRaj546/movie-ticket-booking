1989523f38034d18e62c6ccda71a78e5d767e943
Ayush Raj <ayush04060@gmail.com>
2026-09-02 21:44:02 +0530

    Simple loop – maps SeatType to price.

--- ShowtimeBooking/src/PriceCalculator.java
+++ ShowtimeBooking/src/PriceCalculator.java
@@ -6,6 +6,22 @@ public class PriceCalculator {
     public static final int PLATINUM_PRICE = 400;
     public static double calculateTotal(List<Seat>seats)
     {
-        return 0.0;
+        int totalPrice = 0;
+        for(Seat seat : seats)
+        {
+            switch (seat.getType())
+            {
+                case Silver:
+                    totalPrice += SILVER_PRICE;
+                    break;
+                case Gold:
+                    totalPrice += GOLD_PRICE;
+                    break;
+                case Platinum:
+                    totalPrice += PLATINUM_PRICE;
+                    break;
+            }
+        }
+        return totalPrice;
     }
 }
