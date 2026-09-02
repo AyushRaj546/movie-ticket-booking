92e565ebe136b3a3e83f31d069bb6b66999ee6d6
Ayush Raj <ayush04060@gmail.com>
2026-09-02 21:23:59 +0530

    The Screen composes Seat objects – they are created here and cannot exist without the screen.
    The layout (e.g., 5 rows, 10 columns) is configurable – we’ll use 5×10 in our demo.

--- ShowtimeBooking/src/Screen.java
+++ ShowtimeBooking/src/Screen.java
@@ -5,9 +5,29 @@ public class Screen {
     private int screenNumber;
     private List<Seat> seats;
 
+
     public Screen(int screenNumber, int rows, int columns){
         this.screenNumber = screenNumber;
         this.seats = new ArrayList<>();
+        for(int row = 1; row <= rows; row++)
+        {
+            char rowLetter = (char) ('A' +  row);
+            for (int col = 1; col <= columns; col++)
+            {
+                String seatNum = "" + rowLetter +col;
+                SeatType type;
+                if(row<2)
+                {
+                    type = SeatType.Silver;
+                } else if (row<4) {
+                    type = SeatType.Gold;
+                }
+                else {
+                    type = SeatType.Platinum;
+                }
+                seats.add(new Seat(seatNum, type));
+            }
+        }
     }
     public int getScreenNumber() {
         return screenNumber;
